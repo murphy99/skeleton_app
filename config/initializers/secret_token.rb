07fc27302f265d85879cc04493bb076e7da4e101
Eileen Murphy <e.p.murphy99@gmail.com>
2012-07-19 15:11:06 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkeletonApp::Application.config.secret_token = 'ccac08743c471a5cf469e472aa2cc1ba1e742aceb4d4b124b80e6163c2ab57e67c5b77d89e96417cd636a1a01fb74f3bb18abc0d878c52876989b9169e2c51b8'
