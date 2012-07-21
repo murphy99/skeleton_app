e6394ec9ac9d9e941e99799d59e6f7d4dfcd4631
Eileen Murphy <epm@myplace.com>
2012-07-21 13:21:13 -0400

    Reintializing again and again

---
+++ main_template/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MainTemplatels::Application.config.secret_token = 'a964f381ad4c73980aabc83e3606baa8ff630996b619c17a2f13a7fdd7b156890376451a2cf8778880a4480f7bb319e96a49e2d532e6c6c1ff4dd3f42349f498'
